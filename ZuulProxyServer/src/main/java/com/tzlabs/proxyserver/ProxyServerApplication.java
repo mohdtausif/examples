06c4472b1ebe418ee7645b568e44e9c1c5c1447f
Mohd Tausif <mohd.tausif@power2sme.com>
2020-11-25 18:31:53 +0530

    zuul proxy server

---
+++ ZuulProxyServer/src/main/java/com/tzlabs/proxyserver/ProxyServerApplication.java
@@ -0,0 +1,15 @@
+package com.tzlabs.proxyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ProxyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServerApplication.class, args);
+	}
+
+}
